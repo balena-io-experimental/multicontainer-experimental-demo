55e9afb9f67039f5d45cc66ed6148b031a3a117e
Pablo Carranza Velez <pablo@resin.io>
2017-11-15 01:19:42 -0800

    Fix entrypoints for non-privileged containers, disable foo, change logged strings a bit
    Signed-off-by: Pablo Carranza Velez <pablo@resin.io>

--- foo/index.js
+++ foo/index.js
@@ -9,6 +9,6 @@ app.get('/greeting', function (req, res) {
 var server = app.listen(8081, function () {
 
   var port = server.address().port;
-  console.log('Example app listening on port ', port);
+  console.log('Example app (foo) listening on port ', port);
 
 });
--- main/index.js
+++ main/index.js
@@ -4,16 +4,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  request.getAsync('foo:8081/greeting')
-  .spread (res, body) => {
-    res.send(body);
-  }
+  res.send('Hello world');
+  //request.getAsync("http://foo:8081/greeting").spread((resp, body) => {
+  //  res.send('Proxied from foo: ' + body);
+  //});
 });
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
 
   var port = server.address().port;
-  console.log('Example app listening on port ', port);
+  console.log('Example app (main) listening on port ', port);
 
 });
